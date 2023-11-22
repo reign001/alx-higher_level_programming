75d4a4542a90b3d6a308f0afd1162029566ad49e
reign001 <olochej543@gmail.com>
2023-11-22 13:29:37 -0800

    updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,7 +2,4 @@
 """definesan empty class square"""
 class Square:
     """ a square is an object with four equal sides"""
-    
-    def __init__(self, length, width)
-        self.length = length
-        self.width = width
+    pass
