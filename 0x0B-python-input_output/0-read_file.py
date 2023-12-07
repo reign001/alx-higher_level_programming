bb745a9f932f28b4b1454c3ebd4d6e968a054390
reign001 <olochej543@gmail.com>
2023-12-07 12:54:40 -0800

    file inpout/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def read_file(filename="workfile"):
-    """A function that reads a text file and prints it to stdout"""
-    """Args:
-    filename: the name of the file to be printed
-    """
-    with open('workfile', "r" encoding="utf-8") as f:
-        content = f.read()
-        print(content)
+"""Defines a text file-reading function."""
+
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
