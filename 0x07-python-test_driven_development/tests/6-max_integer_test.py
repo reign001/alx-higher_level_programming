e8ce4339c4d7d6fe9f5749ae728f7638e21ee71f
reign001 <olochej543@gmail.com>
2024-01-18 01:34:29 -0800

    updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python3
 import unittest
-def max_integer(list=[]):
+
+def max_integer(lst=[]):
     """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
+    If the list is empty, the function returns None
     """
-    if len(list) == 0:
+    if len(lst) == 0:
         return None
-    result = list[0]
+    result = lst[0]
     i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
+    while i < len(lst):
+        if lst[i] > result:
+            result = lst[i]
         i += 1
     return result
+
+class TestMaxIntegerFunction(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]), "Empty list should return None")
+
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 5, 3, 8, 2]), 8, "Incorrect result for positive numbers")
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -3, -8, -2]), -1, "Incorrect result for negative numbers")
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 5, -3, 8, -2]), 8, "Incorrect result for mixed numbers")
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([1.5, 2.7, 0.9, 3.2]), 3.2, "Incorrect result for float numbers")
+
+    def test_duplicate_numbers(self):
+        self.assertEqual(max_integer([3, 3, 3, 3]), 3, "Incorrect result for duplicate numbers")
+
 if __name__ == '__main__':
     unittest.main()
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
---
+++ README.md
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+a raedme file for python programing with ALX
+python is a high level programing language which encompases  object oriented programing.
