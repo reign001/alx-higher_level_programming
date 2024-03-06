74a4446c95d5859cf1f905a9a0124557aba58b4b
reign001 <olochej543@gmail.com>
2024-03-06 14:07:20 -0800

    http

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl http://i.imgur.com/z4d4kWk.jpg -i -H "Range: bytes=0-1023"
+
