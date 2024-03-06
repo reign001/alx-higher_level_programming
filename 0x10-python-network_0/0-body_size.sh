e795847ff7f46041af0d026ee9f89ba14245cc3c
reign001 <olochej543@gmail.com>
2024-03-06 15:19:46 -0800

    range

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 #!/bin/bash
-curl -sI http://www.nowhere.com:8000 | grep -i "Range: bytes=0-102" | awk '{print $2}' | tr -d '\r'
+curl -sI http://i.imgur.com/z4d4kWk.jpg -i -H "Range: bytes=0-1023"
+
 
