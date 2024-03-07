565696df12c7571046f3b78b232085f10ec6c0dc
reign001 <olochej543@gmail.com>
2024-03-06 16:42:33 -0800

    GET

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s GET /index.html HTTP/1.0
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+url=$1
+curl -s "$url"
 
