134b179f01a305a39c5d70e7e6859a50e4a05ab1
reign001 <olochej543@gmail.com>
2024-03-06 16:07:58 -0800

    GET

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl GET http://www.example.com -i -H "Range: bytes=0-199"
+curl GET http://www.example.com HTTP/1.0 -i -H "Range: bytes=0-199"
 
