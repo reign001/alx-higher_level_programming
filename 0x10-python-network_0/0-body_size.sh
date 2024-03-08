b8effe901cca8961384fcb8e9f9678c228783e2d
reign001 <olochej543@gmail.com>
2024-03-08 15:31:35 -0800

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl http://www.0.0.0.0:5000/docs/test.txt.(os.stat(\n"/docs/test.txt"))
+curl http://www.nowere.com:500/docs/test.txt(\n.s.stat('/docs/test.txt'))
