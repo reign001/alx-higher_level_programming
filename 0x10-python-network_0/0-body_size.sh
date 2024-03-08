29dc1f91d27e7e55398d3e57538ef84b2db060b0
reign001 <olochej543@gmail.com>
2024-03-08 13:29:40 -0800

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl GET/test.html HTTP/1.0, content-length: 5000
+cURL -x Get http/test.html
