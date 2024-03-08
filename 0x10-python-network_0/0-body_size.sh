bc19d1301a20852e6e1bd5bbd0660670a072376b
reign001 <olochej543@gmail.com>
2024-03-08 12:07:58 -0800

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -I http://i.imgur.com/z4d4kWk.jpg HTTP/1.0
+"""A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response"""
+curl GET/test.html HTTP/1.0.content-length
