8e55b2ef3bb640194fdc44e9654c30864c2a0a46
reign001 <olochej543@gmail.com>
2024-03-23 18:42:02 -0700

    updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-url=$1
-curl -s "$url"
-
+curl -s -L "${1}"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,1 +1,3 @@
-i#!/bin/bash
+#!/bin/bash
+# Sends a GET request to the URL, and displays the body of the response
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
