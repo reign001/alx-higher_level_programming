4432ddaa22c0b20fb4c4a1e9cff2b7f31cf525cf
reign001 <olochej543@gmail.com>
2024-03-27 18:14:55 -0700

    javascript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+function factorial (num) {
+  if (num <= 1) return (1);
+
+  return (factorial(num - 1) * num);
+}
+
+const parsed = parseInt(argv[2]);
+console.log(Number.isNaN(parsed) ? 1 : factorial(parsed));1:wq
+
